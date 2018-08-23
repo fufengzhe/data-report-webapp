0955f14fcb5c222eb50d6cbf8e0bf3ca345602c8
ecdata <xiexiangyu_clec@chinalife.com.cn>
2018-08-23 10:50:56 +0800

    handle null pointer exception

--- src/main/java/cn/com/chinalife/ecdata/service/impl/location/LocationAnalysisServiceImpl.java
+++ src/main/java/cn/com/chinalife/ecdata/service/impl/location/LocationAnalysisServiceImpl.java
@@ -102,31 +102,33 @@ public class LocationAnalysisServiceImpl implements LocationAnalysisService {
             String entity = EntityUtils.toString(response.getEntity(), "utf-8");
             if (entity != null && (!entity.toLowerCase().contains("<!doctype html public"))) {
                 JSONObject jsonObject = JSON.parseObject(entity.replace("__GetZoneResult_ = ", ""));
-                Object locationObject = jsonObject.get("province");
-                Object companyObject = jsonObject.get("catName");
-                String source = codeAndName.get(analysisIndex.getIndexSource()) == null ? analysisIndex.getIndexSource() : codeAndName.get(analysisIndex.getIndexSource());
-                if (locationObject != null) {
-                    String location = locationObject.toString();
-                    if (location != null && location.length() > 0) {
-                        String keyOfLocation = new StringBuffer(analysisIndex.getStatDate()).append("&").append(CommonConstant.statTimeSpanOfDate).append("&").
-                                append(CommonConstant.distributeIndexNameOfRegisterMobile).append("&").append(source).append("&").append("2").append("&").append(location).toString();
-                        Integer locationValue = map.get(keyOfLocation);
-                        if (locationValue == null) {
-                            locationValue = 0;
+                if (jsonObject != null) {
+                    Object locationObject = jsonObject.get("province");
+                    Object companyObject = jsonObject.get("catName");
+                    String source = codeAndName.get(analysisIndex.getIndexSource()) == null ? analysisIndex.getIndexSource() : codeAndName.get(analysisIndex.getIndexSource());
+                    if (locationObject != null) {
+                        String location = locationObject.toString();
+                        if (location != null && location.length() > 0) {
+                            String keyOfLocation = new StringBuffer(analysisIndex.getStatDate()).append("&").append(CommonConstant.statTimeSpanOfDate).append("&").
+                                    append(CommonConstant.distributeIndexNameOfRegisterMobile).append("&").append(source).append("&").append("2").append("&").append(location).toString();
+                            Integer locationValue = map.get(keyOfLocation);
+                            if (locationValue == null) {
+                                locationValue = 0;
+                            }
+                            map.put(keyOfLocation, ++locationValue);
                         }
-                        map.put(keyOfLocation, ++locationValue);
                     }
-                }
-                if (companyObject != null) {
-                    String company = companyObject.toString();
-                    if (company != null && company.length() > 0) {
-                        String keyOfCompany = new StringBuffer(analysisIndex.getStatDate()).append("&").append(CommonConstant.statTimeSpanOfDate).append("&").
-                                append(CommonConstant.distributeIndexNameOfRegisterMobile).append("&").append(source).append("&").append("1").append("&").append(company).toString();
-                        Integer companyValue = map.get(keyOfCompany);
-                        if (companyValue == null) {
-                            companyValue = 0;
+                    if (companyObject != null) {
+                        String company = companyObject.toString();
+                        if (company != null && company.length() > 0) {
+                            String keyOfCompany = new StringBuffer(analysisIndex.getStatDate()).append("&").append(CommonConstant.statTimeSpanOfDate).append("&").
+                                    append(CommonConstant.distributeIndexNameOfRegisterMobile).append("&").append(source).append("&").append("1").append("&").append(company).toString();
+                            Integer companyValue = map.get(keyOfCompany);
+                            if (companyValue == null) {
+                                companyValue = 0;
+                            }
+                            map.put(keyOfCompany, ++companyValue);
                         }
-                        map.put(keyOfCompany, ++companyValue);
                     }
                 }
             }
@@ -148,7 +150,7 @@ public class LocationAnalysisServiceImpl implements LocationAnalysisService {
             String entity = EntityUtils.toString(response.getEntity());
             if (entity != null && (!entity.toLowerCase().contains("<!doctype html public"))) {
                 JSONObject jsonObject = JSON.parseObject(entity);
-                if ("0".equals(jsonObject.getString("code"))) {
+                if (jsonObject != null && ("0".equals(jsonObject.getString("code")))) {
                     JSONObject data = (JSONObject) jsonObject.get("data");
                     Object locationObject = data.get("region");
                     Object companyObject = data.get("isp");
